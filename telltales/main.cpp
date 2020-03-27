e49341a5541a193501ef513cb76a75a51e15463c
Kostiantyn <kvgrygoriev@luxoft.com>
2020-03-27 12:38:11 +0200

    Enable speed meter detection

--- telltales/main.cpp
+++ telltales/main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 #include <vector>
 
+#include "AnalogMeterDetector.h"
 #include "TelltalesDetector.h"
 #include "VideoManager.h"
 
 int main() {
+  const std::vector<std::pair<cv::MorphTypes, std::string>>
+      transformation_type_array{
+          /*
+          {cv::MORPH_ERODE, "MORPH_ERODE"},     // nothing after HoughLinesP
+          {cv::MORPH_HITMISS, "MORPH_HITMISS"}, // nothing after HoughLinesP
+          {cv::MORPH_OPEN, "MORPH_OPEN"},       // nothing after HoughLinesP
+          {cv::MORPH_DILATE, "MORPH_DILATE"},   // a lot of false-positive cases
+                                                // or empty at all
+          {cv::MORPH_GRADIENT, "MORPH_GRADIENT"}, // a lot of false-positive
+                                                  // cases or empty at all
+          {cv::MORPH_BLACKHAT, "MORPH_BLACKHAT"}, // a lot of false-positive
+                                                  // cases or empty at all
+          */
+          {cv::MORPH_CLOSE, "MORPH_CLOSE"},
+          {cv::MORPH_TOPHAT, "MORPH_TOPHAT"}};
+  const cv::Rect kAnalogSpeedMeterCoordinates{-415, -520, 415, 360};
+  AnalogMeterDetector asm_detector(kAnalogSpeedMeterCoordinates,
+                                   cv::MORPH_CLOSE, "MORPH_CLOSE");
+
   TelltalesDetector telltales_detector;
   if (!telltales_detector.GetTellTalesCount()) {
     std::cerr << "Icon list is empty";
@@ -23,8 +43,17 @@ int main() {
     telltales_detector.SetImage(frame);
     telltales_detector.Detect();
 
+    asm_detector.SetImage(frame);
+
+    for (const auto &algorithm : transformation_type_array) {
+      asm_detector.SetMorphTypeTransformation(algorithm.first,
+                                              algorithm.second);
+      // asm_detector.ApplyHoughLines();
+      asm_detector.ApplyHoughLinesP();
+    }
+
     // show live and wait for a key with timeout long enough to show images
-    cv::imshow("Cluster", frame);
+    // cv::imshow("Cluster", frame);
 
     /*
         if (!(frame_index % 50)) {
